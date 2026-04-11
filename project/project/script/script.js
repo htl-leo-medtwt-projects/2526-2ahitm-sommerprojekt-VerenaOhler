b5d747aae5a73d1cbab0ff21e730418177bfb5fa
Verena <v.ohler@students.htl-leonding.ac.at>
2026-04-11 16:50:48 +0200

    Add "Got it!" button to tutorial page and update script for visibility toggle

--- project/project/script/script.js
+++ project/project/script/script.js
@@ -1,6 +1,8 @@
 let previewContainer = document.getElementById("tutorialPreview-container")
+let tutorialContainer = document.getElementById("tutorial-explainationContainer")
 
 function viewTutorial(){
     previewContainer.style.display = "none"
+    tutorialContainer.style.display = "block"
 
 }
