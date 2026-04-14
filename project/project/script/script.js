42eb6ec6da7cf6145d8bd48afde3b3310863f3cc
Verena <v.ohler@students.htl-leonding.ac.at>
2026-04-14 12:09:02 +0200

    Add game screen page and implement navigation; update tutorial and settings for improved functionality

--- project/project/script/script.js
+++ project/project/script/script.js
@@ -4,5 +4,8 @@ let tutorialContainer = document.getElementById("tutorial-explainationContainer"
 function viewTutorial(){
     previewContainer.style.display = "none"
     tutorialContainer.style.display = "block"
+}
 
+function closeSettings(){
+    window.location.href = "../index.html"
 }
