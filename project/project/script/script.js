857c9a97fc798cbaaac1b6e705d1fee6ead03674
Verena <v.ohler@students.htl-leonding.ac.at>
2026-04-16 08:56:40 +0200

    Implement loose state UI: add loose container, buttons, and styles

--- project/project/script/script.js
+++ project/project/script/script.js
@@ -1,6 +1,9 @@
 let previewContainer = document.getElementById("tutorialPreview-container")
 let tutorialContainer = document.getElementById("tutorial-explainationContainer")
 
+let winPage = document.getElementById('win')
+let loosePage = document.getElementById('loose')
+
 function viewTutorial(){
     previewContainer.style.display = "none"
     tutorialContainer.style.display = "block"
@@ -8,4 +11,12 @@ function viewTutorial(){
 
 function closeSettings(){
     window.location.href = "../index.html"
+}
+
+function win(){
+    winPage.style.display = 'block'    
+}
+
+function loose(){
+    loosePage.style.display = 'block'
 }
